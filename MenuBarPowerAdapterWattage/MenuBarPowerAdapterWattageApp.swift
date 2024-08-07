adf61a8e30f0b4590dc2ef38ebdb644253f8579b
Matthew Vandenberg <mv5903@gmail.com>
2024-08-07 10:20:24 -0700

    Update MenuBarPowerAdapterWattageApp.swift

--- MenuBarPowerAdapterWattage/MenuBarPowerAdapterWattageApp.swift
+++ MenuBarPowerAdapterWattage/MenuBarPowerAdapterWattageApp.swift
@@ -1,17 +1,57 @@
-//
-//  MenuBarPowerAdapterWattageApp.swift
-//  MenuBarPowerAdapterWattage
-//
-//  Created by Matthew Vandenberg on 8/7/24.
-//
-
 import SwiftUI
+import IOKit.ps
 
 @main
 struct MenuBarPowerAdapterWattageApp: App {
+    // Hooking the AppDelegate for the app
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        // No main window needed for a menu bar app
+        Settings {
+            EmptyView()
         }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusItem: NSStatusItem!
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        statusItem.button?.title = "...W"
+        
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(quit), keyEquivalent: "q"))
+        statusItem.menu = menu
+
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            self.updateWattage()
+        }
+    }
+    
+    @objc func quit() {
+        NSApplication.shared.terminate(self)
+    }
+
+    func updateWattage() {
+        let wattage = self.getCurrentWattage()
+        if wattage == 0 {
+            statusItem.isVisible = false
+            return
+        }
+        statusItem.isVisible = true
+        statusItem.button?.title = "\(wattage)W"
+    }
+
+    func getCurrentWattage() -> Int {
+        if let adapterDetails = IOPSCopyExternalPowerAdapterDetails()?.takeRetainedValue() as? [String: Any],
+           let watts = adapterDetails["Watts"] as? Int {
+            return watts
+        } else {
+            print("Error: Unable to retrieve power adapter details.")
+        }
+        
+        return 0;
     }
 }
