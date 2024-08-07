79f964c19b8bb3fd2cef792f71358c01aea1b9ac
Matthew Vandenberg <mv5903@gmail.com>
2024-08-07 09:36:33 -0700

    Initial Commit

---
+++ MenuBarPowerAdapterWattage/MenuBarPowerAdapterWattageApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarPowerAdapterWattageApp.swift
+//  MenuBarPowerAdapterWattage
+//
+//  Created by Matthew Vandenberg on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MenuBarPowerAdapterWattageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
